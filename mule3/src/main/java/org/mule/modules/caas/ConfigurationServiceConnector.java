e58d2e3500833c95f7362ddee7ae3659778bbbfd
Juan Alberto López Cavallotti <juancavallotti@gmail.com>
2018-03-09 16:13:38 -0800

    Added SSL configuration to mule 3 client

--- mule3/src/main/java/org/mule/modules/caas/ConfigurationServiceConnector.java
+++ mule3/src/main/java/org/mule/modules/caas/ConfigurationServiceConnector.java
@@ -2,6 +2,7 @@ package org.mule.modules.caas;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import org.apache.commons.lang3.StringUtils;
+import org.glassfish.jersey.SslConfigurator;
 import org.mule.api.MuleContext;
 import org.mule.api.annotations.Config;
 import org.mule.api.annotations.Connector;
@@ -15,14 +16,20 @@ import org.mule.modules.caas.model.ApplicationConfiguration;
 import org.mule.modules.caas.model.ApplicationConfigurationBuilder;
 import org.mule.modules.caas.model.ApplicationDocument;
 import org.mule.transformer.types.SimpleDataType;
+import org.mule.util.ClassUtils;
+import org.mule.util.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.PreferencesPlaceholderConfigurer;
+import sun.net.www.protocol.https.DefaultHostnameVerifier;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collections;
 import java.util.List;
@@ -56,8 +63,7 @@ public class ConfigurationServiceConnector extends PreferencesPlaceholderConfigu
     	logger.debug("Setting up connector with properties: {}", config);
 
     	//standard for rest clients.
-    	Client client = ClientBuilder.newClient();
-    	client.register(JacksonJsonProvider.class);
+		Client client = buildClient();
 
     	ApplicationDataProvider provider = new DefaultApplicationDataProvider(config.getConfigServerBaseUrl(), client);
 
@@ -116,15 +122,73 @@ public class ConfigurationServiceConnector extends PreferencesPlaceholderConfigu
 
         if (doc == null) throw new ConfigurationNotFoundException("Could not find document " + key + " in application " + appConfig.getName());
 
-        Client client = ClientBuilder.newClient();
-        client.register(JacksonJsonProvider.class);
+		Client client = buildClient();
 
         ApplicationDataProvider provider = new DefaultApplicationDataProvider(config.getConfigServerBaseUrl(), client);
 
         return new DefaultTranformingValue<>(provider.loadDocument(doc, appConfig), new SimpleDataType<InputStream>(InputStream.class, doc.getContentType()));
     }
 
-    /**
+	private Client buildClient() {
+		ClientBuilder cb = ClientBuilder.newBuilder();
+        Client client = null;
+
+		try {
+
+            if (config.getTrustStore() == null && config.getKeyStore() == null) {
+                client = cb.build();
+                return client;
+            }
+
+            if (config.isDisableHostNameVerification()) {
+                cb.hostnameVerifier(new HostnameVerifier() {
+                    @Override
+                    public boolean verify(String s, SSLSession sslSession) {
+                        return true;
+                    }
+                });
+            }
+
+            try {
+                //configure SSL if needed.
+                SslConfigurator sslConfig = SslConfigurator.newInstance();
+
+                if (config.getTrustStore() != null) {
+                    logger.debug("Loading trust store from {}", config.getTrustStore());
+                    sslConfig.trustStoreBytes(IOUtils.toByteArray(ClassUtils.getResource(config.getTrustStore(), getClass())));
+                }
+
+                if (config.getTrustStorePassword() != null) {
+                    sslConfig.trustStorePassword(config.getTrustStorePassword());
+                }
+
+                if (config.getKeyStore() != null) {
+                    logger.debug("Loading keystore from {}", config.getKeyStore());
+                    sslConfig.keyStoreBytes(IOUtils.toByteArray(ClassUtils.getResource(config.getKeyStore(), getClass())));
+                }
+
+                if (config.getKeyStorePassword() != null) {
+                    sslConfig.keyStorePassword(config.getKeyStorePassword());
+                }
+
+                cb.sslContext(sslConfig.createSSLContext());
+
+            } catch (IOException ex) {
+                logger.error("Error while configuring SSL on client, leaving client unfonfigured...", ex);
+            }
+
+            client = cb.build();
+
+        } finally {
+            if (client != null) {
+                client.register(JacksonJsonProvider.class);
+            }
+            return client;
+        }
+
+	}
+
+	/**
      * Recursive method to read from the configuration service an app and its parents.
      * @param name the application name to read.
      * @param version the version to read.
--- mule3/src/main/java/org/mule/modules/caas/config/ConnectorConfig.java
+++ mule3/src/main/java/org/mule/modules/caas/config/ConnectorConfig.java
@@ -2,8 +2,11 @@ package org.mule.modules.caas.config;
 
 import org.mule.api.annotations.components.Configuration;
 import org.mule.api.annotations.Configurable;
+import org.mule.api.annotations.display.Password;
+import org.mule.api.annotations.display.Placement;
 import org.mule.api.annotations.param.Default;
 import org.mule.api.annotations.param.Optional;
+
 
 @Configuration(friendlyName = "Configuration Service Connector")
 public class ConnectorConfig {
@@ -13,6 +16,7 @@ public class ConnectorConfig {
 	 */
 	@Configurable
 	@Default("http://localhost:8888/")
+	@Placement(group = "Basic", order = 1)
 	private String configServerBaseUrl;
 	
 	/**
@@ -21,6 +25,7 @@ public class ConnectorConfig {
 	 */
 	@Configurable
     @Optional
+	@Placement(group = "Basic", order = 2)
 	private String applicationName;
 
 
@@ -28,6 +33,7 @@ public class ConnectorConfig {
 	 * The version of the application on the Configuration Service.
 	 */
 	@Configurable
+	@Placement(group = "Basic", order = 3)
 	private String version;
 
 
@@ -35,8 +41,43 @@ public class ConnectorConfig {
 	 * The environment of the application on the configuration service.
 	 */
 	@Configurable
+	@Placement(group = "Basic", order = 4)
 	private String environment;
 
+	/**
+	 * The location of the trust store either in the classpath or in the filesystem.
+	 */
+	@Configurable
+	@Optional @Placement(group = "SSL", tab = "SSL", order = 1)
+	private String trustStore;
+
+	/**
+	 * The password of the trust store.
+	 */
+	@Configurable
+	@Password
+	@Optional @Placement(group = "SSL", tab = "SSL", order = 2)
+	private String trustStorePassword;
+
+	/**
+	 * The location of the keystore either in the classpath or in the filesystem.
+	 */
+	@Configurable
+	@Optional @Placement(group = "SSL", tab = "SSL", order = 3)
+	private String keyStore;
+
+	/**
+	 * The password of the keystore
+	 */
+	@Configurable
+	@Password
+	@Optional @Placement(group = "SSL", tab = "SSL", order = 4)
+	private String keyStorePassword;
+
+	@Configurable
+	@Default("false")
+	@Optional @Placement(group = "SSL", tab = "SSL", order = 5)
+	private boolean disableHostNameVerification;
 
 	public String getApplicationName() {
 		return applicationName;
@@ -68,6 +109,46 @@ public class ConnectorConfig {
 
 	public void setConfigServerBaseUrl(String configServerBaseUrl) {
 		this.configServerBaseUrl = configServerBaseUrl;
+	}
+
+	public String getTrustStore() {
+		return trustStore;
+	}
+
+	public void setTrustStore(String trustStore) {
+		this.trustStore = trustStore;
+	}
+
+	public String getTrustStorePassword() {
+		return trustStorePassword;
+	}
+
+	public void setTrustStorePassword(String trustStorePassword) {
+		this.trustStorePassword = trustStorePassword;
+	}
+
+	public String getKeyStore() {
+		return keyStore;
+	}
+
+	public void setKeyStore(String keyStore) {
+		this.keyStore = keyStore;
+	}
+
+	public String getKeyStorePassword() {
+		return keyStorePassword;
+	}
+
+	public void setKeyStorePassword(String keyStorePassword) {
+		this.keyStorePassword = keyStorePassword;
+	}
+
+	public boolean isDisableHostNameVerification() {
+		return disableHostNameVerification;
+	}
+
+	public void setDisableHostNameVerification(boolean disableHostNameVerification) {
+		this.disableHostNameVerification = disableHostNameVerification;
 	}
 
 	@Override
